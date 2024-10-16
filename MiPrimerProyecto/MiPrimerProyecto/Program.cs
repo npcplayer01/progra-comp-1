9acc8edcdd593de65a4a6ba12e63ed27c4295df5
Oscar Mauricio <dev_deep0@proton.me>
2024-10-15 20:22:30 -0600

    Primer cambio

---
+++ MiPrimerProyecto/MiPrimerProyecto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerProyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Hola mundo, y a la clase de progra 1");
+            Console.ReadLine();
+        }
+    }
+}
